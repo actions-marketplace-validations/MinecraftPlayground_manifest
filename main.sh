b77823f6dc0ea30dcc18baead4f40878bbc79d5e
Mqx <62719703+Mqxx@users.noreply.github.com>
2026-01-05 11:36:51 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -14,6 +14,9 @@
 # release-versions
 # snapshot-versions
 # april-fools-versions
+# type
+# url
+# release-time
 
 manifest_response=$(curl -L $INPUT_MANIFEST_URL)
 
@@ -26,15 +29,23 @@ release_versions=$(echo "$manifest_response" | jq -c '[.versions[] | select(.typ
 snapshot_versions=$(echo "$manifest_response" | jq -c '[.versions[] | select(.type=="snapshot") | .id] | reverse')
 april_fools_versions=$(echo "$manifest_response" | jq -c '[.versions[] | select(.releaseTime | test("-04-01T")) | .id] | reverse')
 
+selected_version=$INPUT_VERSION
+
 if [ "$INPUT_VERSION" == "latest-release" ]; then
   echo "Using latest release version: $latest_release_version"
-  INPUT_VERSION="$latest_release_version"
+  selected_version="$latest_release_version"
 elif [ "$INPUT_VERSION" == "latest-snapshot" ]; then
   echo "Using latest snapshot version: $latest_snapshot_version"
-  INPUT_VERSION="$latest_snapshot_version"
+  selected_version="$latest_snapshot_version"
 else
   echo "Using specified version: $INPUT_VERSION"
 fi
+
+selected_version_object=$(echo "$manifest_response" | jq -c '.versions[] | select(.id=="$selected_version")')
+
+type=$(echo "$selected_version_object" | jq -c '.type')
+url=$(echo "$selected_version_object" | jq -c '.url')
+release_time=$(echo "$selected_version_object" | jq -c '.releaseTime')
 
 echo "versions=$versions" >> "$GITHUB_OUTPUT"
 echo "latest-release-version=$latest_release_version" >> "$GITHUB_OUTPUT"
@@ -42,5 +53,8 @@ echo "latest-snapshot-version=$latest_snapshot_version" >> "$GITHUB_OUTPUT"
 echo "release-versions=$release_versions" >> "$GITHUB_OUTPUT"
 echo "snapshot-versions=$snapshot_versions" >> "$GITHUB_OUTPUT"
 echo "april-fools-versions=$april_fools_versions" >> "$GITHUB_OUTPUT"
+echo "type=$type" >> "$GITHUB_OUTPUT"
+echo "url=$url" >> "$GITHUB_OUTPUT"
+echo "release-time=$release_time" >> "$GITHUB_OUTPUT"
 
 exit 0
